c0bfdd6f93f683d2b8d9a207e79157f3a925969b
srk9110 <65576400+srk9110@users.noreply.github.com>
2020-12-30 15:14:09 +0900

    DB Connect
    DB Connect

---
+++ board/src/main/java/com/board/domain/BoardDTO.java
@@ -0,0 +1,23 @@
+package com.board.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardDTO {
+
+	private Long idx;			//글번호(PK)
+	private String title;		//제목
+	private String content;		//내용
+	private String writer;		//작성자
+	private int viewCnt;		//조회수
+	private String noticeYn;	//공지 여부
+	private String secretYn;	//비밀글 여부
+	private String deleteYn;	//삭제 여부
+	private LocalDateTime insertTime;	//등록일
+	private LocalDateTime updateTime;	//수정일
+	private LocalDateTime deleteTime;	//삭제일
+}
